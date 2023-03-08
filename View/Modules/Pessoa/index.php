520b8a80fbdce7f4271334e66952f1a33145d177
JaquelineBravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-03-08 17:36:09 -0400

    css finalizado

--- View/Modules/Pessoa/index.php
+++ View/Modules/Pessoa/index.php
@@ -5,6 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+
+    <style>
+        body{
+            background-image: linear-gradient(to right, rgb(0, 0, 0), rgb(100, 100, 100));
+            color: #ffffff96;
+        }
+
+        div{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: #000000af;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            border-radius: 15px;
+            padding: 20px;
+        }
+        button {
+    background-color: rgba(53, 53, 53, 0.171);
+    text-decoration: none;
+    padding: 10px 15px;
+    font-size: 15px;
+    display: block;
+    cursor: pointer;
+    margin: 1px;
+    border-radius: 5px;
+    border: none;
+    margin: 5px;
+    color: #ffffff96;
+    }
+    a{
+        text-decoration: none;
+    }
+    </style>
+
 </head>
 <body>
     <div>
